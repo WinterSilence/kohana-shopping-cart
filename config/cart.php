d795fe2a02f566697f3b141b35546851a4c5dd60
Anton <info@ensostudio.ru>
2019-10-16 20:01:29 +0300

    Update cart.php

--- config/cart.php
+++ config/cart.php
@@ -1,10 +1,7 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-	'session' => array(
-		'lifetime'  => Date::DAY,
-		'type'      => Session::$default,
-		'key'       => 'shop_cart',
-	),
-	'model_product' => 'Cart_Product', // ORM adapter for products table
-);
+<?php
+return [
+    'session_group' => 'default',
+    'session_lifetime' => ‭86400‬,
+    'session_key' => 'shopping_cart',
+    'product_model' => 'Model_Cart_Product',
+];
