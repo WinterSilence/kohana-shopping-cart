e54100db217d62ff6161c8c59b186af5a3fda691
Anton <info@ensostudio.ru>
2019-10-19 11:09:18 +0300

    Update cart.php

--- views/widget/cart.php
+++ views/widget/cart.php
@@ -1,17 +1,15 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-<div id="cart_short">
-	<h3><?php echo __('Shopping Cart') ?></h3>
-	<?php if (isset($cart['total'])): ?>
-	<div class="full">
-		<div><?php echo __('Total cost') ?>:<b id="cart_total_cost"><?php echo $cart['total']['cost'] ?></b></div>
-		<?php if ($cart['total']['discount'] > 0): ?>
-		<div><?php echo __('Discount') ?>:<b id="cart_total_discount"><?php echo $cart['total']['discount'] ?></b></div>
+<div id="widget-cart">
+	<h3><?=__('Shopping Cart')?></h3>
+	<?php if ($cart->getItems()): ?>
+	<div class="cart-not-empty">
+		<div><?=__('Amount')?>: <b id="cart-amount"><?=$cart->getAmount()?></b></div>
+		<?php if ($cart->getDiscount()): ?>
+		<div><?=__('Discount')?>: <b id="cart-discount"><?=$cart->getDiscount()?></b></div>
 		<?php endif; ?>
-		<div><?php echo __('Total count') ?>:<b id="cart_total_count"><?php echo $cart['total']['count'] ?></b></div>
-		<div><?php echo HTML::anchor(Route::url('cart'), __('Go to cart')) ?></div>
+		<div><?=__('Count')?>: <b id="cart-count"><?=$cart->getCount()?></b></div>
+		<div><?=HTML::anchor(Route::url('cart'), __('Go to cart'))?></div>
 	</div>
 	<?php else: ?>
-	<div class="empty"><?php echo __('Open cart') ?></div>
+	<div class="cart-empty"><?=__('Empty cart')?></div>
 	<?php endif; ?>
 </div>
